0d64efc12acccc09a6f115ef4e2f9a32c644db90
AbdalrahmanHafez <dahomy999@outlook.sa>
2021-08-30 00:19:49 +0200

    table init

---
+++ src/components/Table.js
@@ -0,0 +1,7 @@
+import { React, useState, useEffect } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Table = () => {};
+
+export default Table;
