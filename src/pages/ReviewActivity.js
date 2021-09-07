d3e6ab99e88487aadea73d6df394f7e062c19018
aboda7am <dahomy999@outlook.sa>
2021-09-07 16:42:21 +0200

    tabs in Review activity

--- src/pages/ReviewActivity.js
+++ src/pages/ReviewActivity.js
@@ -1,38 +1,97 @@
 import { React, useState, useEffect } from "react";
-import TabContext from "@material-ui/lab/TabContext";
-import TabPanel from "@material-ui/lab/TabPanel";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import { DataGrid, AppBar, Tabs, Tab } from "@material-ui/core/";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import {
-  Form,
-  DropdownButton,
-  Dropdown,
-  Button,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 
 const ReviewActivity = () => {
+  const [value, setvalue] = useState(0);
   useEffect(() => {
     // TODO: Fetch all needed to review Activities
   }, []);
+
   const handleButtonClick = (activity) => {
     console.log(activity);
   };
 
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  const handleChange = (event, newvalue) => {
+    setvalue(newvalue);
+  };
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+  const classes = useStyles();
+
   return (
     <>
-      <AppBar position="static">
-        <Tabs aria-label="simple tabs example">
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
-      </AppBar>
-      <TabPanel index={0}>Item One</TabPanel>
-      <TabPanel index={1}>Item Two</TabPanel>
-      <TabPanel index={2}>Item Three</TabPanel>
+      <div className="container my-4">
+        <div
+          className="card mx-auto"
+          style={{ filter: "drop-shadow(0 0 0.2rem #000000)" }}
+        >
+          {/* <div className={classes.root}> */}
+          <AppBar position="static">
+            <Tabs
+              aria-label="simple tabs"
+              value={value}
+              onChange={handleChange}
+            >
+              <Tab label="Waiting" {...a11yProps(0)} />
+              <Tab label="Done" {...a11yProps(1)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel index={0} value={value}>
+            <ActivityBox clickHandler={handleButtonClick} />
+            <hr />
+            <ActivityBox clickHandler={handleButtonClick} />
+            <hr />
+            <ActivityBox clickHandler={handleButtonClick} />
+            <hr />
+            <ActivityBox clickHandler={handleButtonClick} />
+          </TabPanel>
+          <TabPanel index={1} value={value}>
+            Wow Such an Empty place
+          </TabPanel>
+          {/* </div> */}
+        </div>
+      </div>
     </>
   );
 };
